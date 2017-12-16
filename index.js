8f3a7813197836719aecce190e700497190495ba
Anaxirinia Jimenez <anax131@gmail.com>
2017-12-16 19:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 var currentUser = "Cielo";
 var welcomeMessage= "Welcome to Flatbook, " + currentUser + "!"
 var excitedWelcomeMessage =  "WELCOME TO FLATBOOK, " + currentUser.toUpperCase()  + "!"
+var shortGreeting = "Welcome, " + currentUser + "!"
