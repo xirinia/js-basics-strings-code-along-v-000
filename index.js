bb0e92c2e2e6800d85ade9421f21a5b35dcc0abc
Anaxirinia Jimenez <anax131@gmail.com>
2017-12-16 19:02:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+var currentUser = "Cielo";
+var welcomeMessage= "Welcome to Flatbook, " + currentUser + "!"
+var excitedWelcomeMessage =  "WELCOME TO FLATBOOK, " + currentUser.toUpperCase()  + "!"
+var shortGreeting = "Welcome, " + currentUser[0] + "!"
